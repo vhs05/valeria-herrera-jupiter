995820c77f8d724a7e9be3966fd1db3d97a632ed
vhs05 <valerybunny2004@gmail.com>
2025-01-20 13:05:16 -0600

    git commit lesson-11

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let today = new Date ();
+let thisYear = today.getFullYear;
+let footer = document.querySelector('footer');
+let copyright = document.createElement('p');
+copyright.innerHTML = `© Your Name ${thisYear}`;
+footer.appendChild(copyright);
+
+let skills = ["JavaScript", "HTML", "CSS", "GitHub"]
+let skillsSection = document.getElementById('skills');
+let skillsList = skillsSection.querySelector('ul')
+
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement('li')
+    skill.innerText = skills[i]
+    skillsList.appendChild(skill)
+
+}
